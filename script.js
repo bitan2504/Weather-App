a38101ccd9595defd12ac92fceed1993b1b33ec5
Sibsankar-de <sibsankarde910@gmail.com>
2025-02-24 03:30:54 +0530

    Refactor weather display logic and enhance UI styling

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const display = document.getElementById('display');
 const wicon = document.getElementById('wicon');
-const temp = document.getElementById('temp');
-const tempo = document.getElementsByClassName('temporary')[0];
-const wname = document.getElementById('wname');
-const wdesc = document.getElementById('wdesc');
-const cname = document.getElementById('cname');
+const temp = document.getElementById('temparature');
+const pressure = document.getElementById('pressure');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
+const crLocation = document.getElementById('location');
+const weatherBox = document.getElementById('weather-box');
+const initMessage = document.getElementById('init-message');
 
 function getWeather() {
     const API_key = 'ac031c67f89e43c190e8474cc99314ae';
     // const city_name = 'Jangipur';
-    const city_name = document.getElementById('city').value;
-    if (!city) {
+    const city_name = document.getElementById('city-input').value;
+
+    if (!city_name) {
         console.log('Please Enter Something');
         alert('Please Enter something!')
         return;
     }
     const WEATHER_url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`;
     // const FORECAST_url = `https://api.openweathermap.org/data/2.5/forecast?q=${city_name}&appid=${API_key}`;
-    
+
     fetch(WEATHER_url)
         .then(res => res.json())
         .then(data => {
@@ -27,29 +30,18 @@ function getWeather() {
             console.error(err);
             alert("Error fetching url!");
         });
-
-    // fetch(FORECAST_url)
-    //     .then(res => res.json())
-    //     .then(data => {
-    //         displayForecast(data);
-    //     })
-    //     .catch(err => {
-    //         console.error(err);
-    //         alert("Error fetching url!");
-    //     });
 }
 
 function displayWeather(data) {
-    display.style.display = 'grid';
-    temp.innerHTML = `${Math.round(data.main.temp-273)}`+`&#8451`;
-    wname.innerHTML = data.weather[0].main;
-    tempo.style.display = 'none';
-    // wdesc.innerHTML = data.weather[0].description
-    cname.innerHTML = data.name;
+    initMessage.style.display = "none"
+    weatherBox.style.display = "block";
+    temp.innerHTML = `${Math.round(data.main.temp - 273)}`;
+    pressure.innerHTML = data.main.pressure;
+    humidity.innerHTML = data.main.humidity;
+    crLocation.innerHTML = data.name;
+    wind.innerHTML = data.wind.speed;
 
     const iconCode = data.weather[0].icon;
     const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
     wicon.src = iconUrl;
-    wicon.desc = data.weather[0].description;
-    wicon.style.display = 'block';
 }
